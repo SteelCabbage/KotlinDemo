a9066e71746700fba0ce0511252140f90dff6fba
SteelCabbage <liuyi@qingting.fm>
2023-03-10 10:29:02 +0800

    test request permission
    文档地址: (Fragment 这些 API 已废弃，您还在使用吗？)
    https://mp.weixin.qq.com/s/Bu6vMS75kqPTrEGXS2BuMw

--- KotlinFlow/src/main/java/com/example/kotlindemo/MainActivity.kt
+++ KotlinFlow/src/main/java/com/example/kotlindemo/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.kotlindemo
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        testRequestPermission()
+    }
+
+    private fun testRequestPermission() {
+        val tv = findViewById<TextView>(R.id.tv)
+        val registerForActivityResult = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            Log.e("baicai", "registerForActivityResult it=$it")
+        }
+        tv.setOnClickListener {
+            registerForActivityResult.launch(Manifest.permission.READ_PHONE_STATE)
+        }
     }
 }
